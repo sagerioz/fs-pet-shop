8c09a21dd71dff33ef58b52f05538e6fc5731e54
sagerioz <ericaepperson27@yahoo.com>
2017-03-19 13:34:29 -0600

    done

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -15,91 +15,145 @@ app.use(morgan('short'));
 
 app.disable('x-powered-by');
 
-fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
-  let pets = JSON.parse(petsJSON)
+// rebuild:
+app.get('/pets', function(req, res) {
+  fs.readFile('pets.json', 'utf8', (err, petsJSON) =>{
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    let pets = JSON.parse(petsJSON)
+    res.set("Content-Type", "application/json")
+    res.send(JSON.stringify(pets));
+  })
 
-  app.post('/pets', (req, res) => {
-    let pet = req.body;
+});
 
-    if (!pet) {
-      return res.sendStatus(400);
+app.get('/pets/:id', (req, res) => {
+  fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
     }
 
-    pets.push(pet);
+    var id = Number.parseInt(req.params.id);
+    var pets = JSON.parse(petsJSON);
 
-    res.send(pet);
-
-    fs.writeFile(petsPath, JSON.stringify(pets), (err) => {
-      if (err) {
-        throw err;
-      }
-    })
-  });
-
-  app.get('/pets', function(req, res) {
-    res.send(pets);
-  });
-
-  app.get('/pets/:index', function(req, res) {
-    var index = Number.parseInt(req.params.index);
-
-    if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+    if (id < 0 || id >= pets.length || Number.isNaN(id)) {
       return res.sendStatus(404);
     }
 
-    res.send(pets[index]);
+    res.set("Content-Type", "application/json")
+    res.send(JSON.stringify(pets[id]));
   });
+});
+app.post('/pets/', (req, res) => {
+  let pet = req.body;
+  console.log("PET", pet);
+  var age = pet.age
+  var name = pet.name
+  var kind = pet.kind
+  if (!pet || !age || !name || !kind) {
 
-  app.patch('/pets/:index', function(req, res) {
-    var index = Number.parseInt(req.params.index);
-
-    if (Number.isNaN(index) || index < 0 || index >= pets.length) {
-      return res.sendStatus(404);
+    return res.sendStatus(400);
+  }
+  fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
+    if (err){
+      return res.sendStatus(500);
     }
-
-    var pet = req.body;
-
-    if (!pet) {
-      return res.sendStatus(400);
-    }
-
-    pets[index] = pet;
-    console.log("PROCESS", process.argv);
-    res.send(pet);
-
-    fs.writeFile(petsPath, JSON.stringify(pets), (err) => {
-      if (err) {
-        throw err;
-      }
-    })
-
-  });
-  app.delete('/pets/:index', function(req, res) {
-    var index = Number.parseInt(req.params.index);
-
-    if (Number.isNaN(index) || index < 0 || index >= pets.length) {
-      return res.sendStatus(404);
-    }
-
-    var pet = pets.splice(index, 1)[0];
-
+    let pets = JSON.parse(petsJSON)
+    //console.log("PETS", pets);
+    //console.log("PET", pet);
+    pets.push(pet)
     res.send(pet);
     fs.writeFile(petsPath, JSON.stringify(pets), (err) => {
       if (err) {
-        throw err;
+        console.error(err.stack);
+        return res.sendStatus(500);
       }
     })
-  });
-
+  })
 })
+app.patch('/pets/:index', (req, res) => {
+      var index = Number.parseInt(req.params.index);
+      var petDetails = req.body;
+      var age = petDetails.age
+      var name = petDetails.name
+      var kind = petDetails.kind
+      console.log("PET DETAILS", petDetails, "AGE", age);
+      // console.log(age, name, kind)
+      if (!petDetails) {
+        return res.sendStatus(400);
+      }
+        fs.readFile('pets.json', 'utf8', (err, petsJSON) => {
+          if (err){
+            return res.sendStatus(400);
+          }
+          let pets = JSON.parse(petsJSON)
+          if (Number.isNaN(index) || index < 0 || index >= pets.length) {
+            return res.sendStatus(404);
+          }
+          //console.log('Outside For Loop!')
 
-// app.use(function(req, res) {
-//   res.sendStatus(404);
-// });
+            for(var key in pets[index]){
+              //console.log('Inside For Loop');
+              if (age !== undefined && key === 'age') {
+                console.log('age');
+                pets[index].age = age;
+              }
+              if (name !== undefined && key === 'name') {
+                console.log('name');
+                pets[index].name = name;
+              }
+              if (kind !== undefined && key === 'kind') {
+                console.log('kind')
+                pets[index].kind = kind;
+              }
+            }
 
+            //console.log("WHICH PET?", pets[index]);
+
+          res.send(pets[index]);
+          fs.writeFile(petsPath, JSON.stringify(pets), (err) => {
+            if (err) {
+              console.error(err.stack);
+              return res.sendStatus(500);
+            }
+          })
+        })
+
+  });
+  app.delete('/pets/:index', (req, res) => {
+    let index = Number.parseInt(req.params.index);
+    fs.readFile('pets.json', 'utf8', (err, petsJSON) =>{
+      if (err){
+        return res.sendStatus(400);
+      }
+      let pets = JSON.parse(petsJSON)
+      if(Number.isNaN(index) || index < 0 || index >= pets.length){
+        return res.sendStatus(404)
+      }
+      var deletedObj = pets.splice(index, 1)[0]
+      res.send(deletedObj)
+      fs.writeFile(petsPath, JSON.stringify(pets), (err) =>{
+        if (err) {
+          console.error(err.stack);
+          return res.sendStatus(500);
+        }
+      })
+    })
+
+  })
+  app.use(function (req, res, next) {
+   res.status(404).send("Sorry can't find that!")
+  })
 
 app.listen(app.get('port'), function() {
   console.log('Listening on', app.get('port'));
 });
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 module.exports = app;
